bcb896ac990c8789386d56ea3ac946d876c0aa94
DanielZerihunGeda <lenjisa48@gmail.com>
2025-01-12 13:44:17 +0000

    indent

--- app.py
+++ app.py
@@ -4,15 +4,12 @@ import streamlit as st
 def main():
   with st.sidebar:
     openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
-    "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
-    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
-    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
-
-    st.title("Contract Advisory Chatbot")
-    if prompt := st.chat_input():
-      if not openai_api_key:
-          st.info("Please add your OpenAI API key to continue.")
-          st.stop()
+  
+  st.title("Contract Advisory Chatbot")
+  if prompt := st.chat_input():
+    if not openai_api_key:
+        st.info("Please add your OpenAI API key to continue.")
+        st.stop()
 
         
     # Chat interaction section
